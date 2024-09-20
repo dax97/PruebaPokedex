3e9c916429e72fd7f302eec35c2febaa66092e7e
DavidArellano <dax97@live.com.mx>
2024-09-19 22:55:45 -0600

    Vistas Favorites.jsx Home.jsx: esqueletos preparados

---
+++ src/Views/Favorites.jsx
@@ -0,0 +1,17 @@
+//////* librerias y componentes nativos *//////
+/////* componentes de aplicación *//////
+import { Layout } from "../Layout";
+//////* importaciones de custom hooks *//////
+//////* Hojas de estilo *//////
+//////* Importaciones Multimedia *//////
+
+function Favorites()
+{
+    return(
+        <Layout>
+            
+        </Layout>
+    );
+}
+
+export { Favorites }
---
+++ src/Views/Home.jsx
@@ -0,0 +1,17 @@
+//////* librerias y componentes nativos *//////
+/////* componentes de aplicación *//////
+import { Layout } from "../Layout";
+//////* importaciones de custom hooks *//////
+//////* Hojas de estilo *//////
+//////* Importaciones Multimedia *//////
+
+function Home()
+{
+    return(
+        <Layout>
+            
+        </Layout>
+    );
+}
+
+export { Home }
